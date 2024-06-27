a9f6b364053c4619b9ee20d512f45b8c8924d6da
CodePokeX <sonta445@gmail.com>
2024-06-27 13:26:37 +0530

    Assignment 2 ver 2.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const myApp= express();
 myApp.use(express.json());
+const {v4:uuidv4}= require('uuid');
 
 myApp.listen(3000, ()=>{
     console.log("Running on port 3000");
@@ -19,24 +20,26 @@ myApp.post("/user", (req,res)=>{
     const uName=req.body.name;
     const uMail=req.body.email;
     const uAge=req.body.age;
-    if(Object.keys(req.body).length === 0 && req.body.constructor === Object){
+    if(!(req.body.hasOwnProperty("name") && req.body.hasOwnProperty("email") && req.body.hasOwnProperty("age"))){
         res.status(400).send("Please enter name, email and age");
     }
     else{
+        const uId=uuidv4();
         const user={
             "name":uName,
             "email":uMail,
-            "age":uAge
+            "age":uAge,
+            "uid":uId
         };
         const usr=userList.find(usr=>(usr.name===user.name && usr.email===user.email && usr.age===user.age));
         const index=userList.indexOf(usr);
         if(index>-1){
             res.status(400).send(`User already exists!`);
             }
-            else{
-                userList.push(user);
-                res.send(`${JSON.stringify(user)} created successfully!`);
-            }
+        else{
+            userList.push(user);
+            res.send(`${JSON.stringify(user)} created successfully!`);
+        }
 }
 });
 
@@ -44,86 +47,90 @@ myApp.get("/user",(req,res)=>{
     res.send(userList);
 })
 
-myApp.get("/user/:name",(req,res)=>{
-    const searchName=req.params.name;
-        const user=userList.find(usr=> usr.name===searchName);
-        const index=userList.indexOf(user);
-        if(index>-1){
-            res.send(userList[index]);
-        }
-        else{
-            res.status(404).send(`${searchName} not found!`)
-        }
+myApp.get("/user/:uid",(req,res)=>{
+    const searchId=req.params.uid;
+    const user=userList.find(usr=> usr.uid===searchId);
+    const index=userList.indexOf(user);
+    if(index>-1){
+        res.send(userList[index]);
+    }
+    else{
+        res.status(404).send(`User Id: ${searchId} not found!`);
+    }
 });
 
 let userList2=[];
 
 myApp.get("/list",(req,res)=>{
-    res.status(400).send(`Add your name after /list such as /list/<your_name>`);
+    res.status(400).send(`Add your user id after /list such as /list/<your_uid>`);
 });
 
-myApp.get("/list/:name",(req,res)=>{
-    const searchName=req.params.name;
-    const user=userList.find(usr=> usr.name===searchName);
+myApp.get("/list/:uid",(req,res)=>{
+    const searchId=req.params.uid;
+    const user=userList.find(usr=> usr.uid===searchId);
     const index=userList.indexOf(user);
     if(index>-1){
         res.send(userList2[index]);
     }
     else{
-        res.status(404).send(`${searchName} not found!`)
+        res.status(404).send(`User Id: ${searchId} not found!`)
     }
 });
 
 myApp.post("/list",(req,res)=>{
-    res.status(400).send(`Add your name after /list such as /list/<your_name>`);
+    res.status(400).send(`Add your user id after /list such as /list/<your_uid>`);
 });
 
-myApp.post("/list/:name", (req,res)=>{
-    const usrName=req.params.name;
-    if(Object.keys(req.body).length === 0 && req.body.constructor === Object){
+myApp.post("/list/:uid", (req,res)=>{
+    const usrId=req.params.uid;
+    if(!(req.body.hasOwnProperty("item"))){
         res.status(400).send("Please add to your dream list as item");
     }
     else{
-        userList.forEach(user=>{
-            if(usrName===user.name){
-                const index= userList.indexOf(user);
-                if(index>-1){
-                    const item=req.body.item;
-                    if(userList2[index]==null){
-                        userList2[index]=[];
-                        userList2[index].push(item);
-                    }
-                    else{
-                        userList2[index].push(item);
-                    }
-                    res.send(`${item} added successfully!`);
-                }
-                else{
-                    res.status(404).send(`${usrName} not found!`);
-                }
+        const user=userList.find(usr=> usr.uid===usrId);
+        const index=userList.indexOf(user);
+        if(index>-1){
+            const item=req.body.item;
+            if(userList2[index]==null){
+                userList2[index]=[];
+                userList2[index].push(item);
             }
-        })
+            else{
+                userList2[index].push(item);
+            }
+            res.send(`${item} added successfully!`);
+        }
+        else{
+            res.status(404).send(`User Id: ${usrId} not found!`);
+        }
     }    
 });
 
 myApp.put("/list",(req,res)=>{
-    res.send(`Please add the user name and the item to replace as /list/<userName>/<itemName>`);
+    res.status(400).send(`Please add the user id and the item to replace as /list/<userId>/<itemName>`);
 });
 
-myApp.put("/list/:name",(req,res)=>{
-    const searchName=req.params.name;
-    res.send(`Please add the item to be replaced as /list/${searchName}/<itemName>`);
+myApp.put("/list/:uid",(req,res)=>{
+    const searchId=req.params.uid;
+    const user=userList.find(usr=> usr.uid===searchId);
+    const index=userList.indexOf(user);
+    if(index>-1){
+        res.status(400).send(`Please add the item to be replaced as /list/${searchId}/<itemName>`);
+        }
+    else{
+        res.status(404).send(`User Id: ${searchId} not found!`);
+    }
 });
 
-myApp.put("/list/:name/:item", (req,res)=>{
-    const searchName=req.params.name;
+myApp.put("/list/:uid/:item", (req,res)=>{
+    const searchId=req.params.uid;
     const searchItem=req.params.item;
     const newItem=req.body.item;
-    if(Object.keys(req.body).length === 0 && req.body.constructor === Object){
+    if(!(req.body.hasOwnProperty("item"))){
         res.status(400).send("Please add to your dream list as item");
     }
     else{
-        const user=userList.find(usr=> usr.name===searchName);
+        const user=userList.find(usr=> usr.uid===searchId);
         const index=userList.indexOf(user);
         if(index>-1){
             const ind=userList2[index].indexOf(searchItem);
@@ -136,15 +143,15 @@ myApp.put("/list/:name/:item", (req,res)=>{
             }        
         }
         else{
-            res.status(404).send(`${searchName} not found!`);
+            res.status(404).send(`User Id: ${searchId} not found!`);
         }
     }  
 });
 
-myApp.delete("/list/:name/:item",(req,res)=>{
-    const searchName=req.params.name;
+myApp.delete("/list/:uid/:item",(req,res)=>{
+    const searchId=req.params.uid;
     const searchItem=req.params.item;
-    const user=userList.find(usr=> usr.name===searchName);
+    const user=userList.find(usr=> usr.uid===searchId);
     const index=userList.indexOf(user);
     if(index>-1){
         const ind=userList2[index].indexOf(searchItem);
@@ -157,20 +164,20 @@ myApp.delete("/list/:name/:item",(req,res)=>{
         }        
     }
     else{
-        res.status(404).send(`${searchName} not found!`);
+        res.status(404).send(`User Id: ${searchId} not found!`);
     }
 });
 
-myApp.delete("/user/:name",(req,res)=>{
-    const searchName=req.params.name;
-    const user=userList.find(usr=> usr.name===searchName);
+myApp.delete("/user/:uid",(req,res)=>{
+    const searchId=req.params.uid;
+    const user=userList.find(usr=> usr.uid===searchId);
     const index=userList.indexOf(user);
     if(index>-1){
         userList.splice(index,1);
         userList2.splice(index,1);
-        res.send(`${searchName} user has been deleted successfully!`);   
+        res.send(`User Id: ${searchId} user has been deleted successfully!`);   
     }
     else{
-        res.status(404).send(`${searchName} not found!`);
+        res.status(404).send(`User Id: ${searchId} not found!`);
     }
 });
